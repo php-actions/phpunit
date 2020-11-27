94a54475e99234cd0948f6533277fbd1787a02dd
Greg Bowler <greg.bowler@g105b.com>
2020-11-27 14:44:55 +0000

    Remove extra quotes

--- phpunit-action.bash
+++ phpunit-action.bash
@@ -22,62 +22,62 @@ chmod +x "${github_action_path}/phpunit.phar"
 
 if [ -n "$ACTION_CONFIGURATION" ]
 then
-	command_string="$command_string --configuration '$ACTION_CONFIGURATION'"
+	command_string="$command_string --configuration $ACTION_CONFIGURATION"
 fi
 
 if [ -n "$ACTION_LOG_JUNIT" ]
 then
-	command_string="$command_string --log-junit '$ACTION_LOG_JUNIT'"
+	command_string="$command_string --log-junit $ACTION_LOG_JUNIT"
 fi
 
 if [ -n "$ACTION_TESTDOX_HTML" ]
 then
-	command_string="$command_string --testdox-html '$ACTION_TESTDOX_HTML'"
+	command_string="$command_string --testdox-html $ACTION_TESTDOX_HTML"
 fi
 
 if [ -n "$ACTION_TESTDOX_TEXT" ]
 then
-	command_string="$command_string --testdox-text '$ACTION_TESTDOX_TEXT'"
+	command_string="$command_string --testdox-text $ACTION_TESTDOX_TEXT"
 fi
 
 if [ -n "$ACTION_TESTDOX_XML" ]
 then
-	command_string="$command_string --testdox-xml '$ACTION_TESTDOX_XML'"
+	command_string="$command_string --testdox-xml $ACTION_TESTDOX_XML"
 fi
 
 if [ -n "$ACTION_BOOTSTRAP" ]
 then
-	command_string="$command_string --bootstrap '$ACTION_BOOTSTRAP'"
+	command_string="$command_string --bootstrap $ACTION_BOOTSTRAP"
 fi
 
 if [ -n "$ACTION_FILTER" ]
 then
-	command_string="$command_string --filter '$ACTION_FILTER'"
+	command_string="$command_string --filter $ACTION_FILTER"
 fi
 
 if [ -n "$ACTION_TESTSUITE" ]
 then
-	command_string="$command_string --testsuite '$ACTION_TESTSUITE'"
+	command_string="$command_string --testsuite $ACTION_TESTSUITE"
 fi
 
 if [ -n "$ACTION_GROUP" ]
 then
-	command_string="$command_string --group '$ACTION_GROUP'"
+	command_string="$command_string --group $ACTION_GROUP"
 fi
 
 if [ -n "$ACTION_EXCLUDE_GROUP" ]
 then
-	command_string="$command_string --exclude-group '$ACTION_EXCLUDE_GROUP'"
+	command_string="$command_string --exclude-group $ACTION_EXCLUDE_GROUP"
 fi
 
 if [ -n "$ACTION_TEST_SUFFIX" ]
 then
-	command_string="$command_string --test-suffix '$ACTION_TEST_SUFFIX'"
+	command_string="$command_string --test-suffix $ACTION_TEST_SUFFIX"
 fi
 
 if [ -n "$ACTION_WHITELIST" ]
 then
-	command_string="$command_string --whitelist '$ACTION_WHITELIST'"
+	command_string="$command_string --whitelist $ACTION_WHITELIST"
 fi
 
 if [ -n "$ACTION_MEMORY_LIMIT" ]
