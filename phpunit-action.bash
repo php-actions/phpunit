16a0550d998f06a1a37dc4f775196a4c3a9d1334
Greg Bowler <greg.bowler@g105b.com>
2020-12-17 18:12:45 +0000

    Update to use php-build

---
+++ phpunit-action.bash
@@ -0,0 +1,93 @@
+#!/bin/bash
+set -e
+github_action_path=$(dirname "$0")
+docker_tag=$(cat ./docker_tag)
+echo "Docker tag: $docker_tag"
+
+phar_url="https://phar.phpunit.de/phpunit"
+if [ "$ACTION_VERSION" != "latest" ]
+then
+	phar_url="${phar_url}-${ACTION_VERSION}"
+fi
+curl -H "User-agent: cURL (https://github.com/php-actions)" -L "$phar_url" > "${github_action_path}/phpunit.phar"
+chmod +x "${github_action_path}/phpunit.phar"
+
+command_string="phpunit"
+
+if [ -n "$ACTION_CONFIGURATION" ]
+then
+	command_string="$command_string --configuration '$ACTION_CONFIGURATION'"
+fi
+
+if [ -n "$ACTION_LOG_JUNIT" ]
+then
+	command_string="$command_string --log-junit '$ACTION_LOG_JUNIT'"
+fi
+
+if [ -n "$ACTION_TESTDOX_HTML" ]
+then
+	command_string="$command_string --testdox-html '$ACTION_TESTDOX_HTML'"
+fi
+
+if [ -n "$ACTION_TESTDOX_TEXT" ]
+then
+	command_string="$command_string --testdox-text '$ACTION_TESTDOX_TEXT'"
+fi
+
+if [ -n "$ACTION_TESTDOX_XML" ]
+then
+	command_string="$command_string --testdox-xml '$ACTION_TESTDOX_XML'"
+fi
+
+if [ -n "$ACTION_BOOTSTRAP" ]
+then
+	command_string="$command_string --bootstrap '$ACTION_BOOTSTRAP'"
+fi
+
+if [ -n "$ACTION_FILTER" ]
+then
+	command_string="$command_string --filter '$ACTION_FILTER'"
+fi
+
+if [ -n "$ACTION_TESTSUITE" ]
+then
+	command_string="$command_string --testsuite '$ACTION_TESTSUITE'"
+fi
+
+if [ -n "$ACTION_GROUP" ]
+then
+	command_string="$command_string --group '$ACTION_GROUP'"
+fi
+
+if [ -n "$ACTION_EXCLUDE_GROUP" ]
+then
+	command_string="$command_string --exclude-group '$ACTION_EXCLUDE_GROUP'"
+fi
+
+if [ -n "$ACTION_TEST_SUFFIX" ]
+then
+	command_string="$command_string --test-suffix '$ACTION_TEST_SUFFIX'"
+fi
+
+if [ -n "$ACTION_WHITELIST" ]
+then
+	command_string="$command_string --whitelist '$ACTION_WHITELIST'"
+fi
+
+if [ -n "$ACTION_MEMORY_LIMIT" ]
+then
+	command_string="$command_string -d memory_limit=$ACTION_MEMORY_LIMIT"
+fi
+
+if [ -n "$ACTION_ARGS" ]
+then
+	command_string="$command_string $ACTION_ARGS"
+fi
+
+echo "Command: $command_string"
+docker run --rm \
+	--volume "${github_action_path}/phpunit.phar":/usr/local/bin/phpunit \
+	--volume "${GITHUB_WORKSPACE}":/app \
+	--workdir /app \
+	--env COMPOSER_CACHE_DIR="/tmp/composer-cache" \
+	${docker_tag} ${command_string}
